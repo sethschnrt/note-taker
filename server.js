44d956c757b2984940013c50ee30ecdd9b27b9c0
Seth Schoenert <seth.schoenert@gmail.com>
2023-06-01 18:37:29 -0500

    made changes to server.js file paths to see if it would solve my error

--- server.js
+++ server.js
@@ -13,37 +13,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Static directory to be served
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/Develop/public')));
 
 // API routes
 app.get('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8'));
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/Develop/db/db.json'), 'utf8'));
     res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
-    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8'));
+    const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/Develop/db/db.json'), 'utf8'));
     const newNote = req.body;
     newNote.id = uuidv4();
     notes.push(newNote);
-    fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(notes));
+    fs.writeFileSync(path.join(__dirname, '/Develop/db/db.json'), JSON.stringify(notes));
     res.json(newNote);
 });
 
 app.delete('/api/notes/:id', (req, res) => {
     const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8'));
     const newNotes = notes.filter(note => note.id !== req.params.id);
-    fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(newNotes));
+    fs.writeFileSync(path.join(__dirname, '/Develop/db/db.json'), JSON.stringify(newNotes));
     res.json(newNotes);
 });
 
 // HTML routes
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
 });
 
 // Starts the server to begin listening
